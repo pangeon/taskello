f144fa4cf6e628fdf39fb575d9a8009dabee4d6d
pangeon <kamil.cecherz@gmail.com>
2021-06-16 15:59:22 +0200

    Validation in register form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
+from dns.message import Message
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from wtforms.validators import DataRequired, Regexp, ValidationError, Email, EqualTo
 
 from app import data_service_adapter as data
 
@@ -12,12 +13,25 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     e_mail = StringField('E-mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField(
+        'Password', validators=
+            [
+                DataRequired(), 
+                Regexp(
+                    "^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$", 
+                    message='Passwords must have minimum eight characters, at least one letter, one number and one special character'
+                )
+            ]
+        )
     password2 = PasswordField(
-        'Repeat Password', validators=[DataRequired(), EqualTo('password')]
+        'Repeat Password', 
+        validators=[
+            DataRequired(), 
+            EqualTo('password')
+        ]
     )
     submit = SubmitField('Register')
 
     def validate_email(self, email):
-        if data.is_email_exist(e_mail) == True:
+        if data.is_email_exist(email) == True:
             raise ValidationError('Please use a different email address.')
